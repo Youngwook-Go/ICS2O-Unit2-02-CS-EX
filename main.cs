62439f3f422a9d4a1c9db3ef3cb9800b04564f9f
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-11 18:27:20 +0000

    b

--- main.cs
+++ main.cs
@@ -1,13 +1,20 @@
-using System;
+// Created by: Youngwook
+// Created on: OCT 2022
+//
+// This program does basic mathusing System;
 
-class Program 
+using System; 
+
+public class Program 
 {
-  public static void Main (string[] args) 
-  {
-    Console.WriteLine ("6 + 5 = " + (6 + 5));
-    Console.WriteLine ("7 - 3 = " + (7 - 3));
-    Console.WriteLine ("3 + 4 * 2 = " + (3 + 4 * 2));
-    Console.WriteLine ("4 / 2 + 3 = " + (4 / 2 + 3));
-    Console.WriteLine ("\ndone.");
+    
+    public static void Main (string[] args) 
+    {
+        Console.WriteLine ("6 + 5 = " + (6 + 5));
+        Console.WriteLine ("7 - 3 = " + (7 - 3));
+        Console.WriteLine ("3 + 4 * 2 = " + (3 + 4 * 2));
+        Console.WriteLine ("4 / 2 + 3 = " + (4 / 2 + 3));
+        
+        Console.WriteLine ("\ndone.");
   }
 }
