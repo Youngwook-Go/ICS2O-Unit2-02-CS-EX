bd533d0bfd00e1cd01c89e9db988fabb1d66bc23
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-11 18:40:38 +0000

    d

--- main.cs
+++ main.cs
@@ -13,7 +13,6 @@ class Program
         Console.WriteLine("7 - 3 = " + (7 - 3));
         Console.WriteLine("3 + 4 * 2 = " + (3 + 4 * 2));
         Console.WriteLine("4 / 2 + 3 = " + (4 / 2 + 3));
-        
         Console.WriteLine("\ndone.");
     }
 }
