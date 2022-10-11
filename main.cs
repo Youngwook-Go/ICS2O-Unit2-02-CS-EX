c9b21eeb34e353287044c72e13007fd07614a3a8
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-11 18:06:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("6 + 5 = " + (6 + 5));
+    Console.WriteLine ("7 - 3 = " + (7 - 3));
+    Console.WriteLine ("3 + 4 * 2 = " + (3 + 4 * 2));
+    Console.WriteLine ("4 / 2 + 3 = " + (4 / 2 + 3));
+    Console.WriteLine ("\ndone.");
+  }
+}
