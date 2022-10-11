bca1dee4a4df027aa5b15e5d6ffcd1c8f42bba16
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-11 18:12:56 +0000

    a

--- main.cs
+++ main.cs
@@ -1,7 +1,9 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
+class Program 
+{
+  public static void Main (string[] args) 
+  {
     Console.WriteLine ("6 + 5 = " + (6 + 5));
     Console.WriteLine ("7 - 3 = " + (7 - 3));
     Console.WriteLine ("3 + 4 * 2 = " + (3 + 4 * 2));
